216dc16562a2d9c66d22f1099fc9c7cce98cb0a9
Gustavo Gavancho <gustavo.gavancho.l@live.com>
2020-07-13 19:33:23 -0500

    EighteenthCommit

---
+++ PuntoDeVentaDemo.UI.WPF.Administrador/Scripts/MySQL.sql
@@ -0,0 +1,37 @@
+﻿CREATE TABLE `Producto` (
+	`IdProducto` INT NOT NULL AUTO_INCREMENT,
+	`Nombre` varchar(50) NOT NULL UNIQUE,
+	`Costo` DECIMAL(8.2) NOT NULL,
+	PRIMARY KEY (`IdProducto`)
+);
+
+CREATE TABLE `Usuario` (
+	`NombreDeUsuario` varchar(50) NOT NULL,
+	`Nombres` varchar(50) NOT NULL,
+	`Apellidos` varchar(50) NOT NULL,
+	`Password` varchar(50) NOT NULL,
+	PRIMARY KEY (`NombreDeUsuario`)
+);
+
+CREATE TABLE `Venta` (
+	`IdVenta` INT NOT NULL AUTO_INCREMENT,
+	`FechaHora` DATETIME NOT NULL,
+	`Cliente` varchar(50) NOT NULL,
+	`NombreDeUsuario` varchar(50) NOT NULL,
+	PRIMARY KEY (`IdVenta`)
+);
+
+CREATE TABLE `ProductoVendido` (
+	`IdProductoVendido` INT NOT NULL AUTO_INCREMENT,
+	`IdVenta` INT NOT NULL,
+	`Costo` DECIMAL(8.2) NOT NULL,
+	`Cantidad` INT NOT NULL,
+	`IdProducto` INT NOT NULL,
+	PRIMARY KEY (`IdProductoVendido`)
+);
+
+ALTER TABLE `Venta` ADD CONSTRAINT `Venta_fk0` FOREIGN KEY (`NombreDeUsuario`) REFERENCES `Usuario`(`NombreDeUsuario`);
+
+ALTER TABLE `ProductoVendido` ADD CONSTRAINT `ProductoVendido_fk0` FOREIGN KEY (`IdVenta`) REFERENCES `Venta`(`IdVenta`);
+
+ALTER TABLE `ProductoVendido` ADD CONSTRAINT `ProductoVendido_fk1` FOREIGN KEY (`IdProducto`) REFERENCES `Producto`(`IdProducto`);
---
+++ PuntoDeVentaDemo.UI.WPF.Administrador/Scripts/SQLServer.sql
@@ -0,0 +1,66 @@
+﻿CREATE TABLE [Producto] (
+	IdProducto integer identity(1,1) NOT NULL,
+	Nombre varchar(50) NOT NULL UNIQUE,
+	Costo decimal(8,2) NOT NULL,
+  CONSTRAINT [PK_PRODUCTO] PRIMARY KEY CLUSTERED
+  (
+  [IdProducto] ASC
+  ) WITH (IGNORE_DUP_KEY = OFF)
+
+)
+GO
+CREATE TABLE [Usuario] (
+	NombreDeUsuario varchar(50) NOT NULL,
+	Nombres varchar(50) NOT NULL,
+	Apellidos varchar(50) NOT NULL,
+	Password varchar(50) NOT NULL,
+  CONSTRAINT [PK_USUARIO] PRIMARY KEY CLUSTERED
+  (
+  [NombreDeUsuario] ASC
+  ) WITH (IGNORE_DUP_KEY = OFF)
+
+)
+GO
+CREATE TABLE [Venta] (
+	IdVenta integer identity(1,1) NOT NULL,
+	FechaHora datetime NOT NULL,
+	Cliente varchar(50) NOT NULL,
+	NombreDeUsuario varchar(50) NOT NULL,
+  CONSTRAINT [PK_VENTA] PRIMARY KEY CLUSTERED
+  (
+  [IdVenta] ASC
+  ) WITH (IGNORE_DUP_KEY = OFF)
+
+)
+GO
+CREATE TABLE [ProductoVendido] (
+	IdProductoVendido integer identity(1,1) NOT NULL,
+	IdVenta integer NOT NULL,
+	Costo decimal(8,2) NOT NULL,
+	Cantidad integer NOT NULL,
+	IdProducto integer NOT NULL,
+  CONSTRAINT [PK_PRODUCTOVENDIDO] PRIMARY KEY CLUSTERED
+  (
+  [IdProductoVendido] ASC
+  ) WITH (IGNORE_DUP_KEY = OFF)
+
+)
+GO
+
+
+ALTER TABLE [Venta] WITH CHECK ADD CONSTRAINT [Venta_fk0] FOREIGN KEY ([NombreDeUsuario]) REFERENCES [Usuario]([NombreDeUsuario])
+ON UPDATE CASCADE
+GO
+ALTER TABLE [Venta] CHECK CONSTRAINT [Venta_fk0]
+GO
+
+ALTER TABLE [ProductoVendido] WITH CHECK ADD CONSTRAINT [ProductoVendido_fk0] FOREIGN KEY ([IdVenta]) REFERENCES [Venta]([IdVenta])
+ON UPDATE CASCADE
+GO
+ALTER TABLE [ProductoVendido] CHECK CONSTRAINT [ProductoVendido_fk0]
+GO
+ALTER TABLE [ProductoVendido] WITH CHECK ADD CONSTRAINT [ProductoVendido_fk1] FOREIGN KEY ([IdProducto]) REFERENCES [Producto]([IdProducto])
+ON UPDATE CASCADE
+GO
+ALTER TABLE [ProductoVendido] CHECK CONSTRAINT [ProductoVendido_fk1]
+GO
